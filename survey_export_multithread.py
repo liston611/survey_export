163cb03ac5f49435a766bba47ea6e3518b05da1c
liston611 <liston611@gmail.com>
2024-02-13 10:08:35 -0500

    Cleaned up script. Added print statements for when actions execute. Added GUI to execute functions.

--- survey_export_multithread.py
+++ survey_export_multithread.py
@@ -1,10 +1,10 @@
 from arcgis.gis import GIS
-#from arcgis.features import FeatureLayer
 import re
 import os
 import pandas as pd
 from PIL import Image, ExifTags
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 # Compress image
 def compress_img(img_path, img_path_comp, quality = 65):
@@ -34,6 +34,7 @@ def compress_img(img_path, img_path_comp, quality = 65):
     resized_image = image.resize(new_size)
 
     resized_image.save(img_path_comp, quality = quality, optimize=True)
+    print(f"Compressed photo saved at {img_path_comp}")
 
 
 # for feature in features:
@@ -61,14 +62,12 @@ def download_and_rename_attachment(feature, attachment, base_path, comp_path):
         if not os.path.exists(file_path):
             feature_layer.attachments.download(oid=object_id, attachment_id=attachment_id, save_path=folder_path)
             os.rename(os.path.join(folder_path, attachment_name), file_path)
+            print(f"Photo {file_name} saved at {folder_path}")
 
         # Save compressed
         if not os.path.exists(file_path_comp):
             compress_img(file_path, file_path_comp)
 
-        # if os.path.exists(file_path) and os.path.exists(file_path_comp):
-        #     feature_layer.attachments.add(oid=object_id, file_path=file_path_comp)
-        #     feature_layer.attachments.delete(oid=object_id, attachment_id=attachment_id)
 
 def upload_compressed(feature, base_path, comp_path):
     # iterate features
@@ -86,8 +85,73 @@ def upload_compressed(feature, base_path, comp_path):
              file_oid = int(re.search(r"OID(\d{1,4})-", filename)[1])
              if (not filename in attachments_names) and file_oid == object_id:
                  feature_layer.attachments.add(oid=object_id, file_path = os.path.join(file_path_comp,filename))
+                 print(f"Photo {filename} uploaded at {file_path_comp}")
 
-    ## find compressed photos for objectID
+
+def delete_fullres(feature, attachment, base_path, comp_path):
+    object_id = feature.attributes['objectid']
+    creation_date = pd.to_datetime(feature.attributes['CreationDate'], unit='ms')
+    date_str = creation_date.strftime('%m%d%y-%H%M')
+    bus_route = str(feature.attributes['bus_route'])
+    stop_abbr = str(feature.attributes['Abbr'])
+    folder_path = os.path.join(base_path, bus_route, stop_abbr)
+    folder_path_comp = os.path.join(comp_path, bus_route, stop_abbr)
+    os.makedirs(folder_path, exist_ok=True)
+    os.makedirs(folder_path_comp, exist_ok=True)
+
+    attachment_id = attachment['id']
+    attachment_name = attachment['name']
+    file_str, attachment_type = os.path.splitext(attachment_name)
+    file_name = f"{stop_abbr}_{date_str}_OID{object_id}-{attachment_id}{attachment_type}"
+    file_path = os.path.join(folder_path, file_name)
+    
+    # Delete the attachment
+    if not file_str[len(file_str)-5:] == '_comp':
+        if os.path.exists(file_path) and attachment['size'] == os.path.getsize(file_path):
+            feature_layer.attachments.delete(oid=object_id, attachment_id=attachment_id)
+            print(f"Photo {attachment_name} deleted")
+        else:
+            print(f"Bus Stop {stop_abbr} photo does not exist on drive or is different file size. Attachment size: {attachment['size']} Downloaded: {os.path.getsize(file_path)}")
+            
+
+# Use ThreadPoolExecutor to download attachments in parallel
+def execute_download():
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        # Store future tasks
+        future_to_attachment = {executor.submit(
+            download_and_rename_attachment, feature, attachment, 
+            base_path, comp_path): (feature, attachment)
+                                for feature in features for attachment in feature_layer.attachments.get_list(
+                                    oid=feature.attributes['objectid'])}
+        # Process completed futures
+        for future in as_completed(future_to_attachment):
+            future.result()  # You can handle exceptions here or get the result
+
+
+def execute_upload():
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        # Store future tasks
+        future_to_attachment = {executor.submit(
+            upload_compressed, feature, 
+            base_path, comp_path): (feature)
+                                for feature in features
+                                }
+        # Process completed futures
+        for future in as_completed(future_to_attachment):
+            future.result()  # You can handle exceptions here or get the result
+
+
+def execute_delete():
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        # Store future tasks
+        future_to_attachment = {executor.submit(
+            delete_fullres, feature, attachment, 
+            base_path, comp_path): (feature, attachment)
+                                for feature in features for attachment in feature_layer.attachments.get_list(
+                                    oid=feature.attributes['objectid'])}
+        # Process completed futures
+        for future in as_completed(future_to_attachment):
+            future.result()  # You can handle exceptions here or get the result
 
 
 # Your client ID from the registered application
@@ -102,8 +166,6 @@ gis = GIS(org_url, client_id=client_id, redirect_uri='urn:ietf:wg:oauth:2.0:oob'
 
 print("Sign in completed.")
 
-
-# Assuming gis has been authenticated
 item_id = 'a0dc62673ce74a62b574444d4b6ee785'
 item = gis.content.get(item_id)
 feature_layer = item.layers[0]  # Assuming it's the first layer
@@ -115,18 +177,27 @@ features = feature_layer.query(where="1=1", out_fields="*", return_attachments=F
 base_path = 'photos2'
 comp_path = 'photos2\\working'
 
-# Use ThreadPoolExecutor to download attachments in parallel
-with ThreadPoolExecutor(max_workers=6) as executor:
-    # Store future tasks
-    future_to_attachment = {executor.submit(
-        download_and_rename_attachment, feature, attachment, 
-        base_path, comp_path): (feature, attachment)
-                            for feature in features for attachment in feature_layer.attachments.get_list(
-                                oid=feature.attributes['objectid'])}
-    
-    # Process completed futures
-    for future in as_completed(future_to_attachment):
-        future.result()  # You can handle exceptions here or get the result
+import tkinter as tk
+from tkinter import ttk
 
-# for feature in features:
-#     upload_compressed(feature, base_path, comp_path)
+# Initialize the main window
+root = tk.Tk()
+root.title("ArcGIS Operations")
+
+# Set the window size
+root.geometry('300x150')
+
+# Create and place the "Download" button
+download_button = ttk.Button(root, text="Download/Compress Photos", command=execute_download)
+download_button.pack(pady=10)  # Add some vertical padding
+
+# Create and place the "Upload" button
+upload_button = ttk.Button(root, text="Upload Photos", command=execute_upload)
+upload_button.pack(pady=10)  # Add some vertical padding
+
+# Create and place the "Delete" button
+delete_button = ttk.Button(root, text="Delete Hosted Photos", command=execute_delete)
+delete_button.pack(pady=10)  # Add some vertical padding
+
+# Start the GUI event loop
+root.mainloop()
