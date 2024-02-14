9bd38d9bdcae4ec1bde7d03e120a9d9f5e8eb7b7
liston611 <liston611@gmail.com>
2024-02-14 15:08:54 -0500

    created gen_name() function. Created variation of delete function to delete compressed files from AGOL.

--- workforce_export_multithread.py
+++ workforce_export_multithread.py
@@ -36,9 +36,7 @@ def compress_img(file_path, file_path_comp, quality = 65):
     resized_image.save(file_path_comp, quality = quality, optimize=True)
     print(f"Compressed photo saved at {file_path_comp}")
 
-
-# for feature in features:
-def download_and_rename_attachment(feature_layer, feature, attachment, base_path, comp_path):
+def gen_name(feature, attachment):
     object_id = feature.attributes['OBJECTID']
     try:
         creation_date = pd.to_datetime(feature.attributes['inprogressdate'], unit='ms')
@@ -64,6 +62,21 @@ def download_and_rename_attachment(feature_layer, feature, attachment, base_path
             stop_abbr = 'OTHER_'
     else:
         stop_abbr = 'OTHER_'
+
+    attachment_id = attachment['id']
+    attachment_name = attachment['name']
+    _, attachment_type = os.path.splitext(attachment_name)
+    file_name = f"{stop_abbr}_{date_str}_{wrkordr}_OID{object_id}_{attachment_id}{attachment_type}"
+    file_name_comp = f"{stop_abbr}_{date_str}_{wrkordr}_OID{object_id}_{attachment_id}_comp{attachment_type}"
+
+    return [file_name, file_name_comp, stop_abbr]
+
+# for feature in features:
+def download_and_rename_attachment(feature_layer, feature, attachment, base_path, comp_path):
+    object_id = feature.attributes['OBJECTID']
+    
+    file_name, file_name_comp, stop_abbr = gen_name(feature, attachment)
+
     folder_path = os.path.join(base_path, stop_abbr)
     folder_path_comp = os.path.join(comp_path, stop_abbr)
     os.makedirs(folder_path, exist_ok=True)
@@ -71,8 +84,6 @@ def download_and_rename_attachment(feature_layer, feature, attachment, base_path
     attachment_id = attachment['id']
     attachment_name = attachment['name']
     file_str, attachment_type = os.path.splitext(attachment_name)
-    file_name = f"{stop_abbr}_{date_str}_{wrkordr}_OID{object_id}_{attachment_id}{attachment_type}"
-    file_name_comp = f"{stop_abbr}_{date_str}_{wrkordr}_OID{object_id}_{attachment_id}_comp{attachment_type}"
     file_path = os.path.join(folder_path, file_name)
     file_path_comp = os.path.join(folder_path_comp, file_name_comp)
     
@@ -89,76 +100,60 @@ def download_and_rename_attachment(feature_layer, feature, attachment, base_path
             compress_img(file_path, file_path_comp)
 
 
-def upload_compressed(feature_layer, feature, comp_path):
+def upload_compressed(feature_layer, feature, attachment, comp_path):
     # iterate features
     ## find feature OBJECTID
     object_id = feature.attributes['OBJECTID']
+
+    _, file_name_comp, stop_abbr = gen_name(feature, attachment)
+
     attachments = feature_layer.attachments.get_list(oid=object_id)
     attachments_names = [attachment['name'] for attachment in attachments]
 
-    stop_abbr = str(feature.attributes['location'][:6])
-    if len(stop_abbr) == 6:
-        try:
-            if len(str(int(stop_abbr))) != 6: stop_abbr = 'OTHER_'
-        except:
-            stop_abbr = 'OTHER_'
-    else:
-        stop_abbr = 'OTHER_'
     file_path_comp = os.path.join(comp_path, stop_abbr)
 
-    for dirpath, dirnames, filenames in os.walk(file_path_comp, topdown=False):
+    for _, _, filenames in os.walk(file_path_comp, topdown=False):
          for filename in filenames:
-             file_oid = int(re.search(r"_OID(\d{1,4})_", filename)[1])
-             if (not filename in attachments_names) and file_oid == object_id:
+             if (not filename in attachments_names) and filename == file_name_comp:
                  feature_layer.attachments.add(oid=object_id, file_path = os.path.join(file_path_comp,filename))
                  print(f"Photo {filename} uploaded at {file_path_comp}")
 
 
-def delete_fullres(feature_layer, feature, attachment, base_path, comp_path):
+def delete_fullres(feature_layer, feature, attachment, base_path, mode = True):
     object_id = feature.attributes['OBJECTID']
-    try:
-        creation_date = pd.to_datetime(feature.attributes['inprogressdate'], unit='ms')
-    except:
-        creation_date = pd.to_datetime(feature.attributes['inProgressDate'], unit='ms')
-    try:
-        date_str = creation_date.strftime('%m%d%y-%H%M')
-    except:
-        creation_date = pd.to_datetime(feature.attributes['CreationDate'], unit='ms')
-        date_str = creation_date.strftime('%m%d%y-%H%M')
-    date_str = creation_date.strftime('%m%d%y-%H%M')
-    try:
-        wrkordr = str(feature.attributes['workorderid'])
-    except:
-        wrkordr = str(feature.attributes['workOrderId'])
-    if wrkordr == '':
-        wrkordr = 'BLANK'
-    stop_abbr = str(feature.attributes['location'][:6])
-    if len(stop_abbr) == 6:
-        try:
-            if len(str(int(stop_abbr))) != 6: stop_abbr = 'OTHER_'
-        except:
-            stop_abbr = 'OTHER_'
-    else:
-        stop_abbr = 'OTHER_'
+    
+    file_name, _, stop_abbr = gen_name(feature, attachment)
+
     folder_path = os.path.join(base_path, stop_abbr)
-    folder_path_comp = os.path.join(comp_path, stop_abbr)
     os.makedirs(folder_path, exist_ok=True)
-    os.makedirs(folder_path_comp, exist_ok=True)
 
     attachment_id = attachment['id']
     attachment_name = attachment['name']
-    file_str, attachment_type = os.path.splitext(attachment_name)
-    file_name = f"{stop_abbr}_{date_str}_{wrkordr}_OID{object_id}_{attachment_id}{attachment_type}"
+    file_str, _ = os.path.splitext(attachment_name)
     file_path = os.path.join(folder_path, file_name)
     
     # Delete the attachment
-    if not file_str[len(file_str)-5:] == '_comp':
-        if os.path.exists(file_path) and attachment['size'] == os.path.getsize(file_path):
+    if not file_str[len(file_str)-5:] == '_comp' and mode:
+        if os.path.exists(file_path):
+            if attachment['size'] == os.path.getsize(file_path):
+                feature_layer.attachments.delete(oid=object_id, attachment_id=attachment_id)
+                print(f"Photo {attachment_name} deleted")
+            else:
+                print(f"Bus Stop {file_str} is a different file size. Attachment size: {attachment['size']} Downloaded: {os.path.getsize(file_path)}")
+        else:
+            print(f"Bus Stop {file_str} photo does not exist on drive: {file_path}")
+    elif not mode:
+        if os.path.exists(file_path):
+            if attachment['size'] == os.path.getsize(file_path):
+                feature_layer.attachments.delete(oid=object_id, attachment_id=attachment_id)
+                print(f"Photo {attachment_name} deleted")
+            else:
+                print(f"Bus Stop {file_str} is a different file size. Attachment size: {attachment['size']} Downloaded: {os.path.getsize(file_path)}")
+        elif file_str[len(file_str)-5:] == '_comp':
             feature_layer.attachments.delete(oid=object_id, attachment_id=attachment_id)
             print(f"Photo {attachment_name} deleted")
         else:
-            print(f"Bus Stop {stop_abbr} photo does not exist on drive: {os.path.exists(file_path)} or is different file size. Attachment size: {attachment['size']} Downloaded: {os.path.getsize(file_path)}")
-
+            print(f"Bus Stop {file_str} photo does not exist on drive: {file_path}")
     
 
 # Use ThreadPoolExecutor to download attachments in parallel
@@ -168,8 +163,9 @@ def execute_download():
     feature_layer = item.layers[0]  # Assuming it's the first layer
     # Query the feature layer for records
     features = feature_layer.query(where="1=1", out_fields="*", return_attachments=False).features
-
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    
+    max_work = int(workers_entry.get())
+    with ThreadPoolExecutor(max_workers=max_work) as executor:
         # Store future tasks
         future_to_attachment = {executor.submit(
             download_and_rename_attachment, feature_layer, feature, attachment, 
@@ -179,6 +175,7 @@ def execute_download():
         # Process completed futures
         for future in as_completed(future_to_attachment):
             future.result()  # You can handle exceptions here or get the result
+    print('Download/Compress Complete')
 
 
 def execute_upload():
@@ -188,36 +185,43 @@ def execute_upload():
     # Query the feature layer for records
     features = feature_layer.query(where="1=1", out_fields="*", return_attachments=False).features
 
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    max_work = int(workers_entry.get())
+    with ThreadPoolExecutor(max_workers=max_work) as executor:
         # Store future tasks
         future_to_attachment = {executor.submit(
-            upload_compressed, feature_layer, feature, 
-            comp_path): (feature)
-                                for feature in features
-                                }
+            upload_compressed, feature_layer, feature, attachment, 
+            comp_path): (feature, attachment)
+                                for feature in features for attachment in feature_layer.attachments.get_list(
+                                    oid=feature.attributes['OBJECTID'])}
         # Process completed futures
         for future in as_completed(future_to_attachment):
             future.result()  # You can handle exceptions here or get the result
+    print('Upload Complete')
 
 
-def execute_delete():
+def execute_delete(mode = True):
     item_id = item_id_entry.get()
     item = gis.content.get(item_id)
     feature_layer = item.layers[0]  # Assuming it's the first layer
     # Query the feature layer for records
     features = feature_layer.query(where="1=1", out_fields="*", return_attachments=False).features
 
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    max_work = int(workers_entry.get())
+    with ThreadPoolExecutor(max_workers=max_work) as executor:
         # Store future tasks
         future_to_attachment = {executor.submit(
             delete_fullres, feature_layer, feature, attachment, 
-            base_path, comp_path): (feature, attachment)
+            base_path, mode): (feature, attachment)
                                 for feature in features for attachment in feature_layer.attachments.get_list(
                                     oid=feature.attributes['OBJECTID'])}
         # Process completed futures
         for future in as_completed(future_to_attachment):
             future.result()  # You can handle exceptions here or get the result
+    print('Deletion Complete')
 
+def execute_delete_all():
+    execute_delete(mode=False)
+    return
 
 # client ID from the registered application
 from configparser import ConfigParser
@@ -236,7 +240,7 @@ gis = GIS(org_url, client_id=client_id, redirect_uri='urn:ietf:wg:oauth:2.0:oob'
 print("Sign in completed.")
 
 item_id = ''
-
+max_work = 10
 
 # Define base path for saving photos
 base_path = 'HEAT'
@@ -250,27 +254,39 @@ root = tk.Tk()
 root.title("ArcGIS Operations")
 
 # Set the window size
-root.geometry('300x200')
+root.geometry('300x325')
 
 item_id_label = ttk.Label(root, text="Item ID:")
 item_id_label.pack(pady=(10,0))  # Adjust padding as needed
 
-# Create an Entry widget for item_id, initialized with default_item_id
+# Create an Entry widget for item_id, initialized with item_id
 item_id_entry = ttk.Entry(root)
-item_id_entry.insert(0, item_id)  # Pre-fill the Entry with default_item_id
+item_id_entry.insert(0, item_id)  # Pre-fill the Entry with item_id
 item_id_entry.pack(fill = 'x', pady=5)
 
+workers_label = ttk.Label(root, text="Max Workers:")
+workers_label.pack(pady=(10,0))  # Adjust padding as needed
+
+# Create an Entry widget for max_work, initialized with max_work
+workers_entry = ttk.Entry(root)
+workers_entry.insert(0, max_work)  # Pre-fill the Entry with max_work
+workers_entry.pack(pady=5)
+
 # Create and place the "Download" button
-download_button = ttk.Button(root, text="Download/Compress Photos", command=execute_download)
+download_button = ttk.Button(root, text="Download/Compress Pics", command=execute_download)
 download_button.pack(pady=10)  # Add some vertical padding
 
 # Create and place the "Upload" button
-upload_button = ttk.Button(root, text="Upload Compressed Photos", command=execute_upload)
+upload_button = ttk.Button(root, text="Upload Compressed Pics", command=execute_upload)
 upload_button.pack(pady=10)  # Add some vertical padding
 
 # Create and place the "Delete" button
-delete_button = ttk.Button(root, text="Delete Hosted Photos", command=execute_delete)
+delete_button = ttk.Button(root, text="Safe Delete Full-Res (leave compressed)", command=execute_delete)
 delete_button.pack(pady=10)  # Add some vertical padding
+
+# Create and place the "Delete All" button
+delete_all_button = ttk.Button(root, text="Safe Delete All Hosted (including compressed)", command=execute_delete_all)
+delete_all_button.pack(pady=10)  # Add some vertical padding
 
 # Start the GUI event loop
 root.mainloop()
